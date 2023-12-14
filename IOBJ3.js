6a954120b01d2d209eb6d450fdbc059e0de9b0eb
AlejoProgramming244 <142456180+AlejoProgramming244@users.noreply.github.com>
2023-12-13 20:27:32 -0500

    Add files via upload

---
+++ IOBJ3.js
@@ -0,0 +1,114 @@
+status = "";
+obj = [];
+
+document.getElementById('arrowContainer').addEventListener('mouseenter', function() {
+    document.getElementById('navigation').style.left = '0';
+});
+
+document.getElementById('navigation').addEventListener('mouseleave', function() {
+    document.getElementById('navigation').style.left = '-200px';
+});
+
+function setup()
+{
+    Canvas = createCanvas(640, 420);
+    Canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+
+    setTimeout(function()
+    {
+        document.getElementById("status").innerHTML = "Página cargada!";
+
+        
+
+        setTimeout(function()
+        {
+            document.getElementById("status").innerHTML = "Reconociendo Objetos...";
+
+            setTimeout(function()
+            {
+                if(obj.length == 0)
+                {
+                    document.getElementById("status").innerHTML = "No se reconocieron objetos :(";
+                }
+                else
+                {
+                    document.getElementById("status").innerHTML = li;
+
+                    setTimeout(function()
+                    {
+                        document.getElementById("status").innerHTML = ls;
+                    }, 5000)
+                }
+
+            }, 2000);
+
+        }, 3000);
+
+    }, 5000);
+}
+
+function modelLoaded()
+{
+    console.log("Modelo CocoSsd Cargado");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.error("error");
+    }
+    else
+    {
+        console.log(results);
+
+        obj = results;
+    }
+}
+
+function preload()
+{
+    img = loadImage("Cu.jpg");
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+        for(i=0; i < obj.length; i++)
+        {
+            li = "Se detectaron objetos, en una momento llega info, espera unos segundos..."
+
+            ls = "Hay más de 10 objetos en la imagen de la cual el modelo cocosd detectó: " + obj.length;
+
+
+            fill("black");
+
+            objPe = floor(obj[i].confidence * 100);
+
+            text(obj[i].label + " " + objPe + "%", obj[i].x, obj[i].y);
+
+            stroke("black");
+
+            noFill();
+
+            rect(obj[i].x + 10, obj[i].y + 10, obj[i].width, obj[i].height);
+        }
+    }
+    
+    setTimeout(function()
+    {
+        if(status == true)
+        {
+            document.getElementById("status").innerHTML = "No se reconoció ningún objeto :("; 
+        }
+
+    }, 12000);
+}
+
---
+++ main.js
@@ -0,0 +1,24 @@
+function IDHa()
+{
+    window.location = "IOBJ.html";
+}
+
+function IDEs()
+{
+    window.location = "IOBJ1.html";
+}
+
+function IDBo()
+{
+    window.location = "IOBJ2.html";
+}
+
+function IDCu()
+{
+    window.location = "IOBJ3.html";
+}
+
+function IDTM()
+{
+    window.location = "IOBJ4.html";
+}
